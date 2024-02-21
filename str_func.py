21af08164cb82954c78e0d42ce77dee1910b7574
Aleksand Batuev <sankaapelsin@gmail.com>
2024-02-21 20:08:54 +0700

    Feature/Task3

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def grow(value):
+    """Функция, которая меняет все буквы на заглавные"""
     return value.upper()
+
+
+def grow_first(value):
+    """Функция выводит слово, начинабщееся с заглавной буквы"""
+    return value[0].upper() + value[1:]
